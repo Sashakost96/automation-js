ccce409c50a16ca9607a574b37249cb6029e95cc
Oleksandra Kostenko <sashakost96@gmail.com>
2025-10-13 20:57:13 +0300

    updated login & registration test according POM

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,69 +1,88 @@
 /// <reference types="cypress" />
+//npx cypress open --config-file cypress.config.dev.js 
+//npx cypress open --config-file cypress.config.staging.js 
 
 import { loginForm } from "../../pom/forms/LogIn";
-import HomePage, { homePage } from "../../pom/pages/HomePage";
-let testData;
+import { garagePage } from "../../pom/pages/GaragePage";
+
+let validEmail;
+let validPassword;
+let invalidEmail;
+let invalidPassword;
 
 describe("Check Sign In logic", () => {
     beforeEach(() => {
-        cy.fixture('credentials').then((data) => {
-            testData = data;
+        const env = Cypress.env('environment') || 'dev';
+        cy.fixture(`${env}_user.json`).then((data) => {
+            validEmail = data.validCredentials.email;
+            validPassword = data.validCredentials.password;
+            invalidEmail = data.invalidCredentials.email;
+            invalidPassword = data.invalidCredentials.password;
         });
-        cy.visit("/");
+        cy.visit('/');
     });
-    it("Check Sign In with valid credentials", () => {
+
+    it("Check Sign In UI", () => {
         loginForm.openSignInForm().should('be.visible');
         loginForm.formTitle.should('be.visible').and('have.text', 'Log in');
         loginForm.labelEmail.should('be.visible').and('have.text', 'Email');
-        loginForm.emailInput.should('be.visible').type(testData.validCredentials.email).blur();
         loginForm.labelPassword.should('be.visible').and('have.text', 'Password');
-        loginForm.passwordInput.should('be.visible').type(testData.validCredentials.password).blur();
         loginForm.rememberMeLabel.should('be.visible').and('have.text', ' Remember me ');
-        loginForm.rememberMeCheck.should('be.visible').check();
-        loginForm.frorgotPassword.should('be.visible').and('have.text', 'Forgot password');
+        loginForm.rememberMeCheck.should('be.visible');
+        loginForm.forgotPassword.should('be.visible').and('have.text', 'Forgot password');
         loginForm.registrationButton.should('be.visible').and('have.text', 'Registration');
         loginForm.closeButton.should('be.visible').and('have.text', '×');
-        loginForm.loginButton.should('be.enabled').and('have.text', 'Login');
+        loginForm.loginButton.should('not.be.enabled').and('have.text', 'Login');
+        //add checks for colors and styles
+    });
+
+    it("Check Sign In with valid credentials", () => {
+        loginForm.openSignInForm().should('be.visible');
+        loginForm.emailInput.type(validEmail).blur();
+        loginForm.passwordInput.type(validPassword).blur();
+        loginForm.rememberMeCheck.should('be.visible').check();
         loginForm.loginButton.click();
-        cy.wait(3000);
-        homePage.titleH1.should('have.text', 'Garage');
+        garagePage.alertMessage.should('have.text', 'You have been successfully logged in');
+        garagePage.pageTitle.should('have.text', 'Garage');
     });
 
     it("Check Sign In with invalid credentials", () => {
         loginForm.openSignInForm();
-        loginForm.emailInput.type(testData.invalidCredentials.email).blur();
-        loginForm.passwordInput.type(testData.invalidCredentials.password).blur();
+        loginForm.emailInput.type(invalidEmail).blur();
+        loginForm.passwordInput.type(invalidPassword).blur();
         loginForm.loginButton.click();
         loginForm.loginError1.should('have.text', 'Wrong email or password');
         loginForm.loginButton.should('not.be.disabled');
 
-    })
+    });
+
     it("Check Sign In with invalid format of email", () => {
         loginForm.openSignInForm();
         loginForm.emailInput.type('@gmail.com ').blur();
-        loginForm.passwordInput.type(testData.validCredentials.password).blur();
+        loginForm.passwordInput.type(validPassword).blur();
         loginForm.emailError1.should('have.text', 'Email is incorrect');
         loginForm.loginButton.should('be.disabled');
-    })
-     it("Check Sign In without email", () => {
+    });
+
+    it("Check Sign In without email", () => {
         loginForm.openSignInForm();
         loginForm.emailInput.click().blur();
-        loginForm.passwordInput.type(testData.validCredentials.password).blur();
+        loginForm.passwordInput.type(validPassword).blur();
         loginForm.emailError2.should('have.text', 'Email required');
         loginForm.loginButton.should('be.disabled');
-    })
+    });
 
     it("Check Sign In without password", () => {
         loginForm.openSignInForm();
-        loginForm.emailInput.type(testData.validCredentials.email).blur();
+        loginForm.emailInput.type(validEmail).blur();
         loginForm.passwordInput.click().blur();
         loginForm.passwordError1.should('have.text', 'Password required');
         loginForm.loginButton.should('be.disabled');
-    })
+    });
 
     it("Check closing form functionality", () => {
         loginForm.openSignInForm();
         loginForm.closeButton.click();
         loginForm.form.should('not.be.visible');
-    })
+    });
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,43 +1,50 @@
 /// <reference types="cypress" />
 import { registrationForm } from "../../pom/forms/Registration";
 import { homePage } from "../../pom/pages/HomePage";
-let testData;
+let validEmail;
+let validPassword;
+let invalidEmail;
+let invalidPassword;
 
-describe('Check Header navigation', () => {
+describe('Check Registration flows', () => {
     beforeEach(() => {
-         cy.fixture('credentials').then((data) => {
-            testData = data;
+        const env = Cypress.env('environment') || 'dev';
+        cy.fixture(`${env}_user.json`).then((data) => {
+            validEmail = data.validCredentials.email;
+            validPassword = data.validCredentials.password;
+            invalidEmail = data.invalidCredentials.email;
+            invalidPassword = data.invalidCredentials.password;
         });
         cy.visit('/');
     });
-     it("Check Sign Up with valid credentials", () => {
+    it("Check Sign Up with valid credentials", () => {
         registrationForm.openRegistrationForm().should('be.visible');
         registrationForm.formTitle.should('be.visible').and('have.text', 'Registration');
         registrationForm.nameLabel.should('be.visible').and('have.text', 'Name');
         registrationForm.nameInput.should('be.visible').type('Test').blur();
-        registrationForm.lastNameLable.should('be.visible').and('have.text', 'Last name');
+        registrationForm.lastNameLabel.should('be.visible').and('have.text', 'Last name');
         registrationForm.lastNameInput.should('be.visible').type('TestLastN').blur();
         registrationForm.emailLabel.should('be.visible').and('have.text', 'Email');
         registrationForm.emailInput.should('be.visible').type(registrationForm.RndEmail).blur();
         registrationForm.passwordLabel.should('be.visible').and('have.text', 'Password');
         registrationForm.passwordInput.should('be.visible').type('test123KO!').blur();
-        registrationForm.repeatPasswordlabel.should('be.visible').and('have.text', 'Re-enter password');
+        registrationForm.repeatPasswordLabel.should('be.visible').and('have.text', 'Re-enter password');
         registrationForm.repeatPasswordInput.should('be.visible').type('test123KO!').blur();
         registrationForm.closeButton.should('be.visible').and('have.text', '×');
         registrationForm.registerButton.should('be.enabled').and('have.text', 'Register');
-        //cy.wait(5000);
         registrationForm.registerButton.click();
         homePage.titleH1.should('have.text', 'Garage');
     });
-    it.only("Check Sign Up with registered user", () => {
+
+    it("Check Sign Up with registered user", () => {
         registrationForm.openRegistrationForm().should('be.visible');
         registrationForm.nameInput.type('Test').blur();
         registrationForm.lastNameInput.type('TestLastN').blur();
-        registrationForm.emailInput.type(testData.validCredentials.email).blur();
-        registrationForm.passwordInput.type(testData.validCredentials.password).blur();
-        registrationForm.repeatPasswordInput.type(testData.validCredentials.password).blur();
+        registrationForm.emailInput.type(validEmail).blur();
+        registrationForm.passwordInput.type(validPassword).blur();
+        registrationForm.repeatPasswordInput.type(validPassword).blur();
         registrationForm.registerButton.should('be.enabled').and('have.text', 'Register');
         registrationForm.registerButton.click();
-        registrationForm.userExisitError1.should('have.text', 'User already exists');
+        registrationForm.userExistsError1.should('have.text', 'User already exists');
     });
-    });
+});
